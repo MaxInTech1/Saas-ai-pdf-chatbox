247b77cc9c67cb464d8b99e546f3a078729caa91
git-create-devben <benlad636@gmail.com>
2024-07-29 15:42:09 +0100

    solve cors error

--- components/pdfView.tsx
+++ components/pdfView.tsx
@@ -7,7 +7,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Loader2Icon, RotateCw, ZoomInIcon, ZoomOutIcon } from "lucide-react";
-import axios from 'axios';
+import axios from "axios";
 
 //We need to configure CORS
 // gsutil cors set cors.json gs://<saas-chat-pdf>.appspot.com
@@ -20,104 +20,106 @@ import axios from 'axios';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function PdfView({ url }: { url: string }) {
-    const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
-    const [file, setFile] = useState<Blob | null>(null);
-    const [rotation, setRotation] = useState<number>(0);
-    const [scale, setScale] = useState<number>(1);
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [file, setFile] = useState<Blob | null>(null);
+  const [rotation, setRotation] = useState<number>(0);
+  const [scale, setScale] = useState<number>(1);
 
-    useEffect(() => {
-    const fetchFile = async () => { try {
-    const response = await axios.get(url, 
-    {responseType: 'blob', withCredentials: true,
-    }); 
-    setFile(response.data); 
-    } catch(error) { 
-    console.error('Error downloading file:', error);
-    } 
-}; 
-fetchFile(); 
-}, [url]);
+  useEffect(() => {
+    const fetchFile = async () => {
+      try {
+        const response = await axios.get(url, {
+          responseType: "blob",
+          withCredentials: false
+        });
+        setFile(response.data);
+      } catch (error) {
+        console.error("Error downloading file:", error);
+      }
+    };
+    fetchFile();
+  }, [url]);
 
-const onDocumentLoadSuccess = ({ numPages }: { numPages: number }): void => {
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }): void => {
     setNumPages(numPages);
-};
+  };
 
-return (
+  return (
     <div className="flex flex-col justify-center items-center">
-    <div className="sticky top-0 z-50 bg-gray-100 p-2 rounded-b-lg">
+      <div className="sticky top-0 z-50 bg-gray-100 p-2 rounded-b-lg">
         <div className="max-w-6xl px-2 grid grid-cols-6 gap-2">
-        <Button
+          <Button
             variant="outline"
             disabled={pageNumber === 1}
             onClick={() => {
-            if (pageNumber > 1) {
+              if (pageNumber > 1) {
                 setPageNumber(pageNumber - 1);
-            }
+              }
             }}
-        >
+          >
             Previous
-        </Button>
+          </Button>
 
-        <p className="flex items-center justify-center">
+          <p className="flex items-center justify-center">
             {pageNumber} of {numPages}
-        </p>
+          </p>
 
-        <Button
+          <Button
             variant="outline"
             disabled={pageNumber === numPages}
             onClick={() => {
-            if (numPages) {
+              if (numPages) {
                 if (pageNumber < numPages) {
-                setPageNumber(pageNumber + 1);
+                  setPageNumber(pageNumber + 1);
                 }
-            }
+              }
             }}
-        >
+          >
             Next
-        </Button>
+          </Button>
 
-        <Button
+          <Button
             variant="outline"
             onClick={() => setRotation((rotation + 90) % 360)}
-        >
+          >
             <RotateCw />
-        </Button>
+          </Button>
 
-        <Button
+          <Button
             variant="outline"
             disabled={scale >= 1.5}
             onClick={() => {
               setScale(scale * 1.2);
             }}
-        >
+          >
             <ZoomInIcon />
-        </Button>
+          </Button>
 
-        <Button
+          <Button
             variant="outline"
             disabled={scale <= 0.75}
             onClick={() => setScale(scale / 1.2)}
-        >
+          >
             <ZoomOutIcon />
-        </Button>
+          </Button>
         </div>
-    </div>
+      </div>
 
-    {!file ? (
+      {!file ? (
         <Loader2Icon className="animate-spin h-20 w-20 text-purple-900 mt-20" />
-    ) : (
+      ) : (
         <Document
-        loading={null}
-        file={file}
-        rotate={rotation}
-        onLoadSuccess={onDocumentLoadSuccess}
-        className="m-4 overflow-scroll"
+          loading={null}
+          file={file}
+          rotate={rotation}
+          onLoadSuccess={onDocumentLoadSuccess}
+          className="m-4 overflow-scroll"
         >
-        <Page className="shadow-lg" scale={scale} pageNumber={pageNumber} />
+          <Page className="shadow-lg" scale={scale} pageNumber={pageNumber} />
         </Document>
-    )}
+      )}
     </div>
-);
+  );
 }
 export default PdfView;
